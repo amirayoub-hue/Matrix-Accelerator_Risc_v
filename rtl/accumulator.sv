8aa9c2aad91545c2b818218a31cf6928e5a9120a
Amir <mirshekariamirayoub@gmail.com>
2026-07-24 20:45:41 +0330

    Update accumulator.sv

--- rtl/accumulator.sv
+++ rtl/accumulator.sv
@@ -1,1 +1,9 @@
-
+module accumulater
+import matrix_pkg :: *;
+(
+   input logic clk ,rst_n, acc_clear,acc_en ,
+   input logic signed [PROD_WIDTH-1:0] acc_in,
+   output logic signed [ACC_WIDTH-1:0] acc_out 
+);
+    
+endmodule 
