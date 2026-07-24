b8fb94d9af92bdb1b4e947e64607a73dfac0e10e
Amir <mirshekariamirayoub@gmail.com>
2026-07-24 20:55:01 +0330

    Update accumulator.sv

--- rtl/accumulator.sv
+++ rtl/accumulator.sv
@@ -5,5 +5,13 @@ import matrix_pkg :: *;
    input logic signed [PROD_WIDTH-1:0] acc_in,
    output logic signed [ACC_WIDTH-1:0] acc_out 
 );
-    
+    always_ff @( posedge clk or  negedge rst_n ) begin 
+       if(!rst_n) 
+       acc_out <= '0;
+       else if (acc_clear)
+       acc_out <='0;
+       else if (acc_en)
+       acc_out <= acc_out + acc_in;
+        
+  end 
 endmodule 
